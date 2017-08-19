d639db901fd6ad6abc3319241f83f27d87a27d9c
Yan <lyth031@gmail.com>
2017-08-19 15:29:55 +0800

    wrote a python file

---
+++ test1.py
@@ -0,0 +1,29 @@
+begin = 0
+end = 100
+ans = (begin + end) / 2
+
+print("Please think of a number between 0 and 100!")
+print("Is your secret number %d?" % ans)
+
+c = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate \
+the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+
+while c != 'c':
+    while not(c == 'h' or c == 'l' or c == 'c'):
+        print("Sorry, I did not understand your input.")
+        print("Is your secret number %d?" % ans)
+        c = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate \
+the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+    if c == 'c':
+        pass
+    else:
+        if c == 'h':
+            end = ans
+        else:
+            begin = ans
+        ans = (begin + end) / 2
+        print("Is your secret number %d?" % ans)
+        c = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate \
+the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+
+print("Game over. Your secret number was: %d" % ans)
